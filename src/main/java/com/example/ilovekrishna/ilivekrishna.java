fa825b6354dcd8e4b732f79b624a520c9106b9af
MaliYashwant <109430901+MaliYashwant@users.noreply.github.com>
2025-04-18 11:37:22 -0700

    sample update

---
+++ src/main/java/com/example/ilovekrishna/ilivekrishna.java
@@ -0,0 +1,12 @@
+package com.example.ilovekrishna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ilivekrishna {
+    public static void main(String[] args) {
+        SpringApplication.run(ilivekrishna.class, args);
+    }
+}
