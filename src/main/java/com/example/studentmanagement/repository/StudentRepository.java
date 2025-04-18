10a1117eb4c8355ae05fdfd3d129f5dbf74cd22e
MaliYashwant <109430901+MaliYashwant@users.noreply.github.com>
2025-04-18 11:05:27 -0700

    sample update

--- src/main/java/com/example/studentmanagement/repository/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-// Repository Interface
-package com.example.studentmanagement.repository;
-
-import com.example.studentmanagement.model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
